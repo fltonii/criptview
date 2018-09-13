c74ce6ae7d29fa45049a603d3de8288ff5985fb0
imac <imac@imac-lci-13.local>
2018-09-13 17:31:23 -0300

    styling

--- src/components/TopList.js
+++ src/components/TopList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
-import { Spinner } from 'native-base';
+import { TouchableOpacity, StyleSheet } from 'react-native';
+import { Spinner, Thumbnail, View, Text } from 'native-base';
 import { api } from '../Api';
 
 export default class TopList extends Component {
@@ -15,12 +15,44 @@ export default class TopList extends Component {
             console.log('data >>>>', data);
         });
     }
+    
+    renderTop() {
+        let { data } = this.state;
+
+        let ret = [];
+
+        data.Data.map((coin, i) => {
+            ret.push(
+                <TouchableOpacity key={i} style={styles.item}>
+                    <View>
+                        <Thumbnail small source={{ uri: `https://www.cryptocompare.com${coin.CoinInfo.ImageUrl}`}} />
+                    </View>
+                    <View>
+                        <Text>{coin.CoinInfo.Name}</Text>
+                    </View>
+                </TouchableOpacity>
+            );
+        });
+        return ret;
+    }
 
     render(){
+        let { data } = this.state;
+
         return(
             <View>
-                <Spinner />
+                { data ? this.renderTop() : <Spinner /> }
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    item: {
+        backgroundColor: '#dddd',
+        paddingVertical: 8, 
+        alignItems: 'center',
+        marginBottom: 4,
+        justifyContent: 'center',
+    }
+});
